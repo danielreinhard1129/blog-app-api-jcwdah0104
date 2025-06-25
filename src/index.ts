758826e91205a311380577eace3e917baaea0548
Daniel Reinhard <119338658+danielreinhard1129@users.noreply.github.com>
2025-06-25 21:19:04 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import express from "express";
+import { PORT } from "./config/env";
+import { errorMiddleware } from "./middlewares/error.middleware";
+import sampleRouter from "./routes/sample.router";
+
+const app = express();
+
+// config
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/samples", sampleRouter);
+
+// error
+app.use(errorMiddleware);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port : ${PORT}`);
+});
