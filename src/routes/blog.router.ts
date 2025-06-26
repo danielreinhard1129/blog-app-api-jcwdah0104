4cd944b69aa84d50dcf411bf1edff4bea5138415
danielreinhard1129 <danielreinhard1129@gmail.com>
2025-06-26 20:31:13 +0700

    feat: add blog list

--- src/routes/blog.router.ts
+++ src/routes/blog.router.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { createBlogController } from "../controllers/blog.controller";
+import {
+  createBlogController,
+  getBlogsController,
+} from "../controllers/blog.controller";
 
 const router = express.Router();
 
+router.get("/", getBlogsController);
 router.post("/", createBlogController);
 
 export default router;
