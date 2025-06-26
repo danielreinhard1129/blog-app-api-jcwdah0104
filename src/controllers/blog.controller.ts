09ea3937da066cf296ab6ed32cb5f2a9359399c8
Daniel Reinhard <119338658+danielreinhard1129@users.noreply.github.com>
2025-06-26 20:33:34 +0700

    feat: add blog list (#2)

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { createBlogService } from "../services/blog/create-blog.service";
+import { getBlogsService } from "../services/blog/get-blogs.service";
 
 export const createBlogController = async (
   req: Request,
@@ -8,6 +9,27 @@ export const createBlogController = async (
 ) => {
   try {
     const result = await createBlogService(req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getBlogsController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const query = {
+      page: parseInt(req.query.page as string) || 1,
+      take: parseInt(req.query.take as string) || 5,
+      sortOrder: (req.query.sortOrder as string) || "desc",
+      sortBy: (req.query.sortBy as string) || "createdAt",
+      search: (req.query.search as string) || "",
+    };
+
+    const result = await getBlogsService(query);
     res.status(200).send(result);
   } catch (error) {
     next(error);
